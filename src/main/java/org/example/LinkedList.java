5f899e825af90ee74b9822ff584decbc74393369
Gayatri Gurav <gayatri.gurav86@gmail.com>
2024-01-29 23:10:44 -0600

    LinkedList append and removelast method added

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -10,33 +10,67 @@ public class LinkedList {
         int value;
         Node next;
 
-        Node(int value){
+        Node(int value) {
             this.value = value;
         }
     }
 
-    public LinkedList(int value){
+    public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    public void printList(){
+    public void printList() {
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             temp = temp.next;
         }
     }
 
-    public void getHead(){
+    public void getHead() {
         System.out.println("Head:" + head.value);
     }
 
-    public void getTail(){
-        System.out.println("Tail: " +tail.value);
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
     }
-    public void getLength(){
-System.out.println("Length: " + length);
-}
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        length++;
+    }
+
+    //removeLast
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,20 +11,25 @@ public class Main {
 
         //map 1 and map2 pointing to same value here->"pointer"
         HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String,Integer> map2 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
 
-        map1.put("value",11);
+        map1.put("value", 11);
         map2 = map1;
 
         System.out.println(map1);
         System.out.println(map2);
 
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(5);
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
 
-        myLinkedList.printList();
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast());
+
+        // myLinkedList.printList();
 
     }
 
