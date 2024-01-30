59e67270e0115519c65ed17a00d7cb26696d7852
Gayatri Gurav <gayatri.gurav86@gmail.com>
2024-01-30 12:43:19 -0600

    added methods in linkedList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.w3c.dom.Node;
-
 import java.util.HashMap;
 
 public class Main {
@@ -11,7 +9,7 @@ public class Main {
 
         //map 1 and map2 pointing to same value here->"pointer"
         HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String, Integer> map2 = new HashMap<>();
+        HashMap<String, Integer> map2;
 
         map1.put("value", 11);
         map2 = map1;
@@ -21,13 +19,19 @@ public class Main {
 
         LinkedList myLinkedList = new LinkedList(2);
         myLinkedList.append(5);
+        myLinkedList.prepend(1);
+        myLinkedList.set(2,8);
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
+        myLinkedList.insert(0,9);
+        myLinkedList.remove(0);
 
-        System.out.println(myLinkedList.removeLast().value);
-        System.out.println(myLinkedList.removeLast().value);
-        System.out.println(myLinkedList.removeLast());
+       // System.out.println(myLinkedList.removeLast().value);
+       // System.out.println(myLinkedList.removeLast().value);
+       // System.out.println(myLinkedList.removeLast());
+        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.get(1).value+ "\n");
 
         // myLinkedList.printList();
 
