e205bfa918b2e2bde142260e3c28559d0d8999c5
Gayatri Gurav <gayatri.gurav86@gmail.com>
2024-02-13 23:21:13 -0600

    New commit for DS queations

--- src/main/datastructures/AmazonInterviewQuestions/ReverseWordsInaSentense.java
+++ src/main/datastructures/AmazonInterviewQuestions/ReverseWordsInaSentense.java
@@ -4,4 +4,20 @@ public class ReverseWordsInaSentense {
 
     //Reverse the order of words in a given sentence(an array of characters)
     //"Hello World"-> "World Hello"
+    public static String reverseWords(String s) {
+
+        String[] words = s.split(" +");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length-1;i>=0;i--){
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args){
+       ReverseWordsInaSentense reverseWordsInaSentense = new ReverseWordsInaSentense();
+        String s = "Hello World";
+        System.out.println(reverseWordsInaSentense.reverseWords(s));
+    }
 }
